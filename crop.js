264c49b2cefc78bbc3fc4ee5d6a450be2873e5aa
233k <0h23k1.n40k1@gmail.com>
2019-09-06 16:22:10 +0900

    refactor: cropper

--- crop.js
+++ crop.js
@@ -1,6 +1,6 @@
 const execSync = require('child_process').execSync;
 
-function cropOption(input, start, duration, x, y, w=128, h=128) {
+function cropOption(input, start, duration, x, y, w, h) {
   const option = `-y -i ${input} -ss ${start} -t ${duration} \
                  -filter_complex "crop=${w}:${h}:${x}:${y}"`;
   return option;
@@ -9,4 +9,5 @@ function cropOption(input, start, duration, x, y, w=128, h=128) {
 function cropExec(option, output) {
   const command = `ffmpeg ${option} ${output}`
   execSync(command);
+  console.log('Done.');
 }
