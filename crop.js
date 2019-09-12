c8888ed46b06571503cbb44ea0379889a654d313
233k <0h23k1.n40k1@gmail.com>
2019-09-12 16:11:16 +0900

    feat: add fps and scale options

--- crop.js
+++ crop.js
@@ -1,8 +1,8 @@
 const execSync = require('child_process').execSync;
 
-function cropOption(input, start, duration, x, y, w, h) {
+function cropOption(input, start, duration, x, y, w, h, fps=30, scale=-1) {
   const option = `-y -i ${input} -ss ${start} -t ${duration} \
-                 -filter_complex "crop=${w}:${h}:${x}:${y}"`;
+                 -filter_complex "crop=${w}:${h}:${x}:${y},fps=${fps},scale=${scale}:-1"`;
   return option;
 }
 
