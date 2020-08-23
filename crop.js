d5f2d8f51d30346b715390dc065c2d388ffd91eb
zeek0x <0h23k1.n40k1@gmail.com>
2020-08-24 04:02:42 +0900

    fix: crop option

--- crop.js
+++ crop.js
@@ -2,8 +2,8 @@ const statSync = require('fs').statSync;
 const execSync = require('child_process').execSync;
 
 function cropOption(input, start, duration, x, y, w, h, fps, scale) {
-  const option = `-y -i ${input} -ss ${start} -t ${duration} \
-                 -filter_complex "crop=${w}:${h}:${x}:${y},fps=${fps},scale=${scale}:-1"`;
+  const option = `-y -ss ${start} -t ${duration} -i ${input} \
+                 -filter_complex "crop=${w}:${h}:${x}:${y},fps=${fps},yadif,scale=${scale}:-1"`;
   return option;
 }
 
